1ff9e782a403d7f0e835452bca1da632a57c1ba7
Sungmin Kim <purpleblues568@gmail.com>
2016-07-06 19:46:56 +0900

    added xcode project

---
+++ xcode/sqlite_test/sqlite_test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sqlite_test
+//
+//  Created by 김성민 on 2016. 7. 6..
+//  Copyright © 2016년 Purple. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
