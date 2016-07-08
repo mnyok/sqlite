092b0b3b9828b10802d3544cf4dfd85c8f4d42b2
Sungmin Kim <purpleblues568@gmail.com>
2016-07-08 19:27:55 +0900

    added common source

---
+++ common/common.h
@@ -0,0 +1,28 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include <time.h>
+#include "../sqlite3.h"
+
+
+#define nil NULL
+
+#define check() \
+                \
+    if(rc != SQLITE_OK){\
+        puts(sqlite3_errmsg(db)); \
+        puts("sqlite close"); \
+        sqlite3_close(db); \
+        exit(1);\
+    }
+
+
+
+int sql_execute(sqlite3* db, const char * sql);
+
+int sql_insert_rand(sqlite3* db, const char * table);
+int sql_update_rand(sqlite3* db, const char * table);
