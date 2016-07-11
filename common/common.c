8f3c885467a7ea907c28bafc75359fa807ca68a3
Ubuntu <orc1226@naver.com>
2016-07-11 10:18:57 +0000

    Merge branch 'ms' of https://github.com/purpleblues/sqlite into sungmin
    * 'ms' of https://github.com/purpleblues/sqlite:
      cherrypick common files
      added common source
      making test code
      save mxFrame value in mj
      초기세팅
      master branch
    
    # Conflicts:
    #	common/common.c
    #	common/common.h
    #	wal_test/main.c
    #	wal_test/test1-wal.db
    #	wal_test/test2-wal.db

--- common/common.c
+++ common/common.c
@@ -9,11 +9,68 @@
 
 int sql_execute(sqlite3* db, const char * sql){
     int rc;
-    
+
     rc = sqlite3_exec(db, sql, nil, nil, nil);
-    
+
     check();
-    
+
+    return rc;
+}
+
+//insert into table values (a, b, c)
+int sql_insert(sqlite3* db, const char * table, int a, int b, int c){
+    int i;
+    int rc;
+    char buffer[100];
+    char sql[1000] = "insert into ";
+
+    strcat(sql, table);
+    strcat(sql, " values (");
+
+    sprintf(buffer, "%d", a);
+    strcat(sql, buffer);
+    strcat(sql, ",");
+
+    sprintf(buffer, "%d", b);
+    strcat(sql, buffer);
+    strcat(sql, ",");
+
+    sprintf(buffer, "%d", c);
+    strcat(sql, buffer);
+    strcat(sql, ")");
+
+    // printf("%s\n", sql);
+
+    rc = sqlite3_exec(db, sql, nil, nil, nil);
+
+    check();
+
+    return rc;
+}
+
+//update table set b = b + value where a = key
+int sql_update(sqlite3* db, const char * table, int key, int value){
+    int i;
+    int rc;
+    char buffer[100];
+    char sql[1000] = "update ";
+
+    strcat(sql, table);
+    strcat(sql, " set b = b");
+
+    sprintf(buffer, "%+d", value);
+    strcat(sql, buffer);
+
+    strcat(sql, " where a = ");
+    sprintf(buffer, "%d", key);
+    strcat(sql, buffer);
+
+    // printf("%s\n", sql);
+
+    rc = sqlite3_exec(db, sql, nil, nil, nil);
+
+    check();
+
     return rc;
 }
 
@@ -22,10 +79,10 @@ int sql_insert_rand(sqlite3* db, const char * table){
     int i;
     char buffer[100];
     char sql[1000] = "insert into ";
-    
+
     strcat(sql, table);
     strcat(sql, " values (");
-    
+
     for(i = 0; i < 3; i++){
         sprintf(buffer, "%d", rand() % 100);
         strcat(sql, buffer);
@@ -34,9 +91,9 @@ int sql_insert_rand(sqlite3* db, const char * table){
         }
     }
     strcat(sql, ")");
-    
+
     rc = sql_execute(db,sql);
-    
+
     return rc;
 }
 
@@ -46,16 +103,16 @@ int sql_update_rand(sqlite3* db, const char * table){
     //    int i;
     char buffer[100];
     char sql[1000] = "update ";
-    
+
     strcat(sql, table);
     strcat(sql, " set a = ");
-    
+
     sprintf(buffer, "%d", rand() % 100);
     strcat(sql, buffer);
-    
+
     // printf("%s\n", sql);
-    
+
     rc = sql_execute(db,sql);
-    
+
     return rc;
 }
