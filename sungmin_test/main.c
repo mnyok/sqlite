63f8ce5b61dffdf6a62353dfc5eb3e1918a2b878
Sungmin Kim@Linux <purpleblues568@gmail.com>
2016-07-12 10:01:27 +0000

    added sungmin_test

---
+++ sungmin_test/main.c
@@ -0,0 +1,49 @@
+//
+//  main.c
+//  sqlite_test
+//
+//  Created by 김성민 on 2016. 7. 6..
+//  Copyright © 2016년 Purple. All rights reserved.
+//
+
+#include "../common/common.h"
+
+
+
+#define nil NULL
+int main(){
+    
+    
+    int rc;
+    int i;
+    
+    srand((unsigned)time(NULL) + (unsigned)getpid());
+    
+    sqlite3* db;
+    rc = sqlite3_open_v2("test1-wal.db",&db,SQLITE_OPEN_READWRITE,nil);
+    
+    check();
+    
+    
+    sql(db,"attach database 'test2-wal.db' as t2");
+    
+    for(i = 0; i < 1; i++){
+        sql(db,"begin transaction");
+    
+        sql_insert_rand(db, "tb1");
+        sql_insert_rand(db, "tb1");
+        sql_insert_rand(db, "t2.tb2");
+        sql_insert_rand(db, "t2.tb2");
+        
+        sql_update_rand(db, "tb1");
+
+        sql(db,"commit transaction");
+    }
+    
+    sql(db,"delete from tb1");
+    sql(db,"delete from t2.tb2");
+    
+    sqlite3_close(db);
+    return 0;
+}
+
